e171cdff385bba6875339f289501a68603e4de0e
Norbert Crombach <norbert.crombach@primetheory.org>
2009-03-30 07:10:20 +0200

    Finish SAX parser

--- lib/plistr.rb
+++ lib/plistr.rb
@@ -1,74 +1,147 @@
 require 'nokogiri'
-require 'time'
+require 'date'
 require 'base64'
 
-class Plistr
-  class Value
-    def initialize(element)
-      @element = element
+module Plistr
+  class Document < Nokogiri::XML::SAX::Document
+    VALUES = %w(string real integer date data key)
+    BOOLEANS = %w(true false)
+
+    attr_reader :value
+
+    def start_document
+      @stack = []
+      @text = ""
+    end
+
+    def start_element(name, attributes)
+      return if name == "plist"
+
+      if VALUES.include?(name)
+        @stack.push Value.new(name)
+      elsif BOOLEANS.include?(name)
+        @stack.push Boolean.new(name)
+      elsif name == "array"
+        @stack.push Array.new
+      elsif name == "dict"
+        @stack.push Dict.new
+      end
+    end
+
+    def characters(string)
+      @text.concat string
+    end
+
+    def end_element(name)
+      return unless value = @stack.pop
+
+      if value.is_a?(Value)
+        value.text.concat @text.strip
+      end
+      @text = ""
+
+      if @stack.last.is_a?(Struct)
+        @stack.last.push value
+      elsif @stack.empty?
+        @value = value.parse
+      end
+    end
+  end
+
+  class Element
+  end
+
+  class Value < Element
+    attr_reader :name, :text
+
+    def initialize(name)
+      @name = name
+      @text = ""
     end
 
     def parse
-      case @element.name
+      case @name
       when "string"
-        @element.text
+        @text
       when "real"
-        @element.text.to_f
+        @text.to_f
       when "integer"
-        @element.text.to_i
-      when "true"
-        true
-      when "false"
-        false
+        @text.to_i
       when "date"
-        Time.iso8601(@element.text)
+        DateTime.parse(@text)
       when "data"
-        Base64.decode64(@element.text.gsub(/\s/, ''))
-      when "array"
-        parse_array
-      when "dict"
-        parse_dict
+        Base64.decode64(@text.gsub(/\s/, ''))
+      when "key"
+        @text
       end
     end
-
-    private
-      def parse_array
-        values = @element.xpath('*').map { |element| Value.new(element) }
-        values.map { |value| value.parse }
-      end
-
-      def parse_dict
-        elements = @element.xpath('*')
-
-        dict = {}
-        key = nil
-        elements.each do |element|
-          if key.nil?
-            key = element.text
-          else
-            dict[key] = Value.new(element).parse
-            key = nil
-          end
-        end
-
-        dict
-      end
   end
 
-  class << self
-    def read(filename)
-      data = File.read(filename)
+  class Boolean < Element
+    def initialize(value)
+      @value = value
+    end
+
+    def parse
+      @value == "true"
+    end
+  end
+
+  class Struct < Element
+  end
+
+  class Array < Struct
+    def initialize
+      @array = []
+    end
+
+    def parse
+      @array
+    end
+
+    def push(value)
+      @array.push value.parse
+    end
+  end
+
+  class Dict < Struct
+    def initialize
+      @hash = {}
+    end
+
+    def parse
+      @hash
+    end
+
+    def push(value)
+      if !@key
+        @key = value.text
+      else
+        @hash[@key] = value.parse
+        @key = nil
+      end
+    end
+  end
+
+  class Reader
+    def self.open(filename)
+      data = File.open(filename)
       new(data)
     end
-  end
 
-  def initialize(data)
-    @document = Nokogiri::XML.parse(data)
-    root = @document.xpath('/plist/*').first
-    @value = Value.new(root)
-  end
+    def initialize(data)
+      @data = data
+    end
 
-  def parse
-    @value.parse
+    def value
+      @value || parse
+    end
+
+    def parse
+      @document = Document.new
+      parser = Nokogiri::XML::SAX::Parser.new(@document)
+      parser.parse(@data)
+      @value = @document.value
+    end
   end
 end
